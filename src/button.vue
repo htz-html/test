32484374def16215f096d37337df594c47d214c6
htz-html <1006676722@qq.com>
2021-05-07 23:30:06 +0800

    验证icon的位置

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,50 @@
 <template>
-  <button>按钮</button>
+  <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {};
+export default {
+  // props:['icon','iconPosition']
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        //属性检查器，检查传过来的值是否正确
+        return !(value !== "left" && value !== "right");
+      },
+    },
+  },
+};
 </script>
-<style lang='scss'>
+<style lang='scss' scoped>
+.g-button {
+  padding: 0 1em;
+  height: 30px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+    }
+  }
+}
 </style>
