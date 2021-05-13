37ebf043deff7f0b69ea78b9987874b5ca08de98
htz-html <1006676722@qq.com>
2021-05-13 23:21:25 +0800

    使用vue测试工具  请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -4,7 +4,7 @@ import sinonChai from 'sinon-chai'
 chai.use(sinonChai)
 
 
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import Button from "../../src/button"
 import Vue from 'vue'
 
@@ -13,17 +13,17 @@ describe('demo.vue', () => {
   it('存在.', () => {
     expect(Button).to.exist
   })
+  // 测试属性（使用wrapper）
   it("可以设置icon.", () => {
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-      propsData: {
-        icon: "settings",
-      },
-    }).$mount();
-    const useElement = vm.$el.querySelector("use");
-    expect(useElement.getAttribute("xlink:href")).to.equal("#i-settings");
-    vm.$destroy();
+    const wrapper = mount(Button, {
+      propsData:{
+        icon: 'search'
+      }
+    })
+    const useElement = wrapper.find("use");
+    expect(useElement.attributes()['href']).to.equal("#i-search");
   });
+  // 测试属性（没有使用wrapper）
   it("可以设置loading.", () => {
     const Constructor = Vue.extend(Button);
     const vm = new Constructor({
@@ -37,6 +37,7 @@ describe('demo.vue', () => {
     expect(useElements[0].getAttribute("xlink:href")).to.equal("#i-loading");
     vm.$destroy();
   });
+  // 测试css
   it("icon 默认的 order 是 1", () => {
     const div = document.createElement("div");
     document.body.appendChild(div);
@@ -51,14 +52,14 @@ describe('demo.vue', () => {
     vm.$el.remove();
     vm.$destroy();
   });
+  // 测试事件
   it("点击 button 触发 click 事件", () => {
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-      propsData: {
-        icon: "settings",
-      },
-    }).$mount();
-
+    const wrapper = mount(Button, {
+      propsData:{
+        icon: 'search'
+      }
+    })
+    const vm = wrapper.vm
     const callback = sinon.fake();
     vm.$on("click", callback);
     vm.$el.click();
